3bced907219b58aaaf997fff443682cef4501cba
jeff <>
2022-08-31 12:19:45 -0400

    added react router, components, removed some boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Homepage";
+import About from "./components/Homepage";
+import Login from "./components/Homepage";
+import Navbar from "./components/Homepage";
+import Footer from "./components/Homepage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
+        <Routes>
+              <Route path = "/" element={<Home />}></Route>
+              <Route path = "about" element={<About />}></Route>
+              <Route path = "contact" element={<Login />}></Route>
+              <Route path = "checkout" element={<Footer />}></Route>
+        </Routes>
+      <Footer />
       </header>
     </div>
   );
