12776f47d805cfed8d50853ccbcec9673026926f
jeff <>
2022-09-12 16:05:40 -0400

    added contact path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from "./components/Home";
-import About from "./components/About";
+import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Search from "./components/Search";
+import Profile from "./components/Profile"; 
+import Logout from "./components/Logout"; 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,10 +19,12 @@ function App() {
       <Navbar />
         <Routes>
               <Route path = "/" element={<Home />}></Route>
-              <Route path = "/about" element={<About />}></Route>
+              <Route path = "/contact" element={<Contact />}></Route>
               <Route path = "/login" element={<Login />}></Route>
               <Route path = "/register" element={<Register />}></Route>
               <Route path = "/search" element={<Search />}></Route>
+              <Route path = "/profile" element={<Profile />}></Route>
+              <Route path = "/logout" element={<Logout />}></Route>
         </Routes>
       <Footer />
       </header>
