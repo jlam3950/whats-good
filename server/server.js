f70bcc0fecac908838c12de4276a73c2616e521d
jeff <>
2022-09-24 10:33:28 -0400

    updated for heroku

--- server/server.js
+++ server/server.js
@@ -54,11 +54,11 @@ require("./passport-config")(passport);
 //serve front end
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, '../src/build')));
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../src/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'src', 'build', '/index.html')
+      path.resolve(__dirname, '../', 'src', 'build', './index.html')
     )
   );
 } else {
