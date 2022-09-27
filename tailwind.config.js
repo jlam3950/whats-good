e89aba27f8a75df2d5a243cf9d2511f7e7dcf90e
jeff <>
2022-09-27 18:17:21 -0400

    updated dependencies for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  },
+  purge: [],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
